a2698d21300c26648f14ab6f6f1f02dfe7dc7290
ljfa-ag <ljfa-ag@web.de>
2015-10-19 22:30:54 +0200

    Refactor ItemLogic

--- src/main/scala/de/ljfa/advbackport/logic/ItemLogic.scala
+++ src/main/scala/de/ljfa/advbackport/logic/ItemLogic.scala
@@ -2,17 +2,8 @@ package de.ljfa.advbackport.logic
 
 import net.minecraft.block.Block
 import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NBTTagList
 import net.minecraftforge.common.util.Constants
-import net.minecraft.item.ItemBlock
-import net.minecraftforge.common.IPlantable
-import net.minecraft.item.ItemDoor
-import net.minecraft.item.ItemReed
-import net.minecraft.item.ItemBed
-import net.minecraft.item.ItemSkull
-import net.minecraft.item.ItemSign
-import net.minecraft.item.ItemRedstone
-import net.minecraft.item.Item
-import net.minecraft.init.Items
 
 object ItemLogic {
     def canDestroy(tool: ItemStack, block: Block): Boolean = checkList("CanDestroy", tool, block)
@@ -22,12 +13,12 @@ object ItemLogic {
         stack.getTagCompound match {
             case null => false
             case tag if tag.hasKey(listName, Constants.NBT.TAG_LIST) => {
-                val nbtList = tag.getTagList(listName, Constants.NBT.TAG_STRING)
-                val list = for(i <- 0 until nbtList.tagCount) yield nbtList getStringTagAt i
-                
+                val list = asStringList(tag.getTagList(listName, Constants.NBT.TAG_STRING))
                 list exists { Block.getBlockFromName(_) eq block }
             }
             case _ => false
         }
     }
+    
+    def asStringList(nbtList: NBTTagList) = for(i <- 0 until nbtList.tagCount) yield nbtList getStringTagAt i
 }
